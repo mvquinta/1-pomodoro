285b3d1365c51f1111e817505dbd10131d32791d
mvquinta <mvqdev@gmail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --get user.namgit config --global user.email mvqdev@gmail.com>
2021-05-21 13:21:12 +0200

    added edit todo task feature

---
+++ src/components/PopEditTodoText.js
@@ -0,0 +1,93 @@
+import React from 'react'
+
+const styles = {
+    container: {
+        position: 'relative',
+    },
+    popupedit: {
+        color: '#2d2d2d',
+        boxSizing: 'border-box',
+        position: 'absolute',
+        width: '220px',
+        height:'220px',
+        left: '15%',
+        transform: 'translateY(-34px)',
+        borderRadius: '20px',
+        border: '4px solid #F2636F',
+        backgroundColor: '#F2E7DC',
+        textAlign: 'center',
+        fontSize: '24px',
+        paddingTop:'10%',
+    },
+    popbuttons: {
+        background: 'none',
+        border: 'none',
+        fonSize: '1.125rem',
+        cursor: 'pointer',
+        color: '#2d2d2d',
+        marginTop:'20px',
+
+        "&:hover": {
+            color: '#F2E7DC',
+            border: '2px solid red',
+        },
+    },
+    popinput: {
+        border: 'none',
+        backgroundColor: '#FFF5EC',
+        width: '80px',
+        marginTop:'20px',
+        marginLeft:'10px',
+        fontSize: '1.125rem',
+        textAlign: 'center',
+    },
+}
+
+export default function PopEditTodoText(props) {
+
+    const [newTodoText, setNewTodoText] = React.useState('')
+
+    /*function editTodo(id) {
+        const updateTodos = [...props.valueTodos].map((todo) => {
+            if(todo.id === id) {
+                todo.text = newTodoText
+            }
+            return todo
+        })
+        props.valueSetTodos(updateTodos)
+        //add a setTodoEdit state to false?
+    }*/
+
+    function handleOkClick (id) {
+        setNewTodoText(newTodoText)
+        const updateTodos = [...props.valueTodos].map((todo) => {
+            if(todo.id === id) {
+                todo.text = newTodoText
+            }
+            return todo
+        })
+        props.valueSetTodos(updateTodos)
+        props.valueSetToggleEditTodo(false)
+    }
+
+    //sets toggle to false closing the window
+    function handleCancelClick() {
+        props.valueSetToggleEditTodo(false)
+    }
+
+    return(
+        <div style={styles.container}>
+            <div style={styles.popupedit}>
+                <label>Edit Todo Task</label>
+                <input 
+                type='text'
+                placeholder='edit task'
+                onChange={(e) => setNewTodoText(e.target.value)}
+                style={styles.popinput}
+                />
+                <button style={styles.popbuttons} onClick={() => handleOkClick(props.valueId)}>OK</button>
+                <button style={styles.popbuttons} onClick={handleCancelClick}>Cancel</button>
+            </div>
+        </div>
+    )
+}
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -6,15 +6,20 @@ import { IconContext } from 'react-icons'
 import Hovertip from './Hovertip'
 import PopEditProjectName from './PopEditProjectName'
 import PopSwitchProj from './PopSwitchProj'
+import PopEditTodoText from './PopEditTodoText'
 
 export default function Todos() {
 
     const [todos, setTodos] = useState([]) //state of todos
-    const [input, setInput] = useState('') //state of input in the form-input to add new todo. Input will then become text in the todo object created    
+    const [input, setInput] = useState('') //state of input in the form-input to add new todo. Input will then become text in the todo object created
+    //const [updateTodoText, setUpdateTodoText] = useState('')
     const [activeProjectName, setActiveProjectName] = useState('1+Pomodoro Project') //state used to tell the UI to only render the todos that belong to the active project
     const [mergedProjectNames, setMergedProjectNames] = useState(['1+Pomodoro Project']) //state of merged projects. Since I put all my todos on the same list. Many of them belong to a repeated project name. This merges and a creates an array with only one element, one proj name
+
     const [toggleProjectName, setToggleProjectName] = useState(false) //state of toggles to popwindow or close it
     const [toggleSwitchProj, setToggleSwitchProj] = useState(false) //state of toggles to popwindow or close it
+    const [toggleEditTodo, setToggleEditTodo] = useState(false) //state of toggles to popwindow or close it
+
 
     //generateId is a "wild" function to create a random id. Mainly used to add an id to every todo created.
     function generateId() {
@@ -36,6 +41,21 @@ export default function Todos() {
     //removeTodo, filters all todos by id and creates a new array with all todos that DO NOT match the passed id
     const removeTodo = (id) => setTodos((todos) => todos.filter((t) => t.id !== id))
 
+    /*function editTodo(id) {
+        const updateTodos = [...todos].map((todo) => {
+            if(todo.id === id) {
+                todo.text = updateTodoText
+            }
+            return todo
+        })
+        setTodos(updateTodos)
+        setUpdateTodoText('')
+        //add a setTodoEdit state to false?
+    }
+
+    useEffect(() => {
+    },[updateTodoText])*/
+
     //useEffect on mergedProjectNames is used iterate trough all todos and check what project names exists.
     //everytime it finds a repeated project he ignores it, when he finds a new one he adds it to the array mergedProjectNames
     //this array mergedProjectNames will than be used by PopSwitchProjs as a list of options
@@ -50,6 +70,10 @@ export default function Todos() {
     }
     function togglePopSwitchProj() {
         toggleSwitchProj ? setToggleSwitchProj(false) : setToggleSwitchProj(true)
+    }
+
+    function togglePopEditTodo() {
+        toggleEditTodo ? setToggleEditTodo(false) : setToggleEditTodo(true)
     }
 
     return(
@@ -79,6 +103,12 @@ export default function Todos() {
                 <div className='project-todos-title-position'>
                     <h3>Todo List</h3>
                 </div>
+                {toggleEditTodo ? <PopEditTodoText
+                                valueTodos={todos}
+                                valueSetTodos={setTodos}
+                                valueSetToggleEditTodo={setToggleEditTodo}
+                                
+                                /> : null}
                 <ul className='todos-list'>
                     {todos.filter((todo) => todo.project === activeProjectName).map(({ text, id }) => (
                         <li key={id}>
@@ -87,8 +117,8 @@ export default function Todos() {
                                 {/*<button><IoCheckbox /></button>*/}
                                 <span>{text}</span>
                             </div>
-                            <div>
-                                <button> <IoPencil /> </button>
+                            <div>         
+                                <button onClick={togglePopEditTodo}><IoPencil /> </button>
                                 <button onClick={() => removeTodo(id)}> <IoTrash /></button>                                
                             </div>
                         </li>
