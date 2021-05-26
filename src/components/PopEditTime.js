577b8664d278a2e100e838f0e06978ee030f65a7
mvquinta <mvqdev@gmail.comgit config --global init.defaultBranch maingit config --global color.ui autogit config --get user.namgit config --global user.email mvqdev@gmail.com>
2021-05-26 11:37:06 +0200

    updated styling

--- src/components/PopEditTime.js
+++ src/components/PopEditTime.js
@@ -1,84 +1,133 @@
-import React from 'react'
+import React from "react";
 
 const styles = {
-    container: {
-        position: 'relative',
-    },
-    popupedit: {
-        color: '#2d2d2d',
-        boxSizing: 'border-box',
-        position: 'absolute',
-        width: '220px',
-        height:'220px',
-        left: '15%',
-        transform: 'translateY(-34px)',
-        borderRadius: '20px',
-        border: '4px solid #F2636F',
-        backgroundColor: '#F2E7DC',
-        textAlign: 'center',
-        fontSize: '24px',
-        paddingTop:'10%',
-    },
-    popbuttons: {
-        background: 'none',
-        border: 'none',
-        fonSize: '1.125rem',
-        cursor: 'pointer',
-        color: '#2d2d2d',
-        marginTop:'20px',
+  container: {
+    position: "relative",
+    zIndex: "100"
+  },
+  popupedit: {
+    color: "#F2E7DC",
+    boxSizing: "border-box",
+    position: "absolute",
+    width: "350px",
+    height: "350px",
+    left: "-50%",
+    transform: "translateY(-80px)",
+    borderRadius: "40px",
+    backgroundColor: "#F25C5C",
+    textAlign: "center",
+    fontSize: "24px"
+  },
+  editContainer: {
+    backgroundColor: "#2D2D2D",
+    borderRadius: "40px",
+    border: "4px solid #F25C5C",
+    height: "80%"
+  },
+  poph2: {
+    padding: "50px 0 30px 0"
+  },
+  popInputContainer: {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "center",
+    gap: "30px"
+  },
+  popLabel: {
+    padding: "20px"
+  },
+  popinput: {
+    border: "none",
+    color: "#F2E7DC",
+    backgroundColor: "#404040",
+    borderRadius: "6px",
+    width: "120px",
+    fontSize: "1.25rem",
+    textAlign: "center"
+  },
+  popButtonsContainer: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    gap: "25px",
+    color: "#2D2D2D"
+  },
+  popbuttons: {
+    background: "none",
+    border: "none",
+    fonSize: "1.125rem",
+    cursor: "pointer",
+    color: "#2D2D2D",
+    margin: "15px"
+  }
+};
 
-        "&:hover": {
-            color: '#F2E7DC',
-            border: '2px solid red',
-        },
-    },
-    popinput: {
-        border: 'none',
-        backgroundColor: '#FFF5EC',
-        width: '80px',
-        marginTop:'20px',
-        marginLeft:'10px',
-        fontSize: '1.125rem',
-        textAlign: 'center',
-    },
-}
+const overlayStyles = {
+  position: "fixed",
+  top: "0",
+  left: "0",
+  right: "0",
+  bottom: "0",
+  backgroundColor: "rgba(0,0,0,0.5",
+  zIndex: "100"
+};
 
 export default function PopEditTime(props) {
+  const [editTime, setEditTime] = React.useState(0);
+  const [secondsEditTime, setSecondsEdiTime] = React.useState(0);
 
-    const [editTime, setEditTime] = React.useState(0)
-    const [secondsEditTime, setSecondsEdiTime] = React.useState(0)
+  //reads the input fields and converts it to seconds. This value is than used to update the setSession time back in ClockTimer.js
+  function handleOkClick() {
+    const convertedTime = parseInt(editTime) * 60 + parseInt(secondsEditTime);
+    props.valueSetSession(convertedTime);
+    props.valueSetPopEditTime(false);
+  }
 
-    //reads the input fields and converts it to seconds. This value is than used to update the setSession time back in ClockTimer.js
-    function handleOkClick () {
-        const convertedTime = parseInt(editTime) * 60 + parseInt(secondsEditTime)
-        props.valueSetSession(convertedTime)
-        props.valueSetPopEditTime(false)
-    }
+  function handleCancelClick() {
+    props.valueSetPopEditTime(false);
+  }
 
-    function handleCancelClick() {
-        props.valueSetPopEditTime(false)
-    }
-
-
-    return(
-        <div style={styles.container}>        
-            <div style={styles.popupedit}>
-                <label>Minutes</label>
+  return (
+    <>
+      <div style={overlayStyles} />
+      <div style={styles.container}>
+        <div style={styles.popupedit}>
+          <div style={styles.editContainer}>
+            <h2 style={styles.poph2}>Edit Time</h2>
+            <div style={styles.popInputContainer}>
+              <div>
+                <label style={styles.popLabel}>Minutes</label>
                 <input
-                type='number'
-                onChange={(e) => setEditTime(e.target.value)}
-                placeholder='25'
-                style={styles.popinput} />
-                <label>Seconds</label>
+                  type="number"
+                  onChange={(e) => setEditTime(e.target.value)}
+                  placeholder="25"
+                  style={styles.popinput}
+                />
+              </div>
+              <div>
+                <label style={styles.popLabel}>Seconds</label>
                 <input
-                type='number'
-                min='0'
-                onChange={(e) => setSecondsEdiTime(e.target.value)}
-                placeholder='0'
-                style={styles.popinput} />
-                <button style={styles.popbuttons} onClick={handleOkClick}>OK</button>
-                <button style={styles.popbuttons} onClick={handleCancelClick}>Cancel</button>
+                  type="number"
+                  min="0"
+                  onChange={(e) => setSecondsEdiTime(e.target.value)}
+                  placeholder="0"
+                  style={styles.popinput}
+                />
+              </div>
             </div>
+          </div>
+          <div style={styles.popButtonsContainer}>
+            <button style={styles.popbuttons} onClick={handleOkClick}>
+              OK
+            </button>
+            <div>|</div>
+            <button style={styles.popbuttons} onClick={handleCancelClick}>
+              Cancel
+            </button>
+          </div>
         </div>
-    )
+      </div>
+    </>
+  );
 }
